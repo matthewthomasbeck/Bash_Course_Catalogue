aa5eb338a3598d30c8acb7228890885167b0b134
matthewthomasbeck <matthewthomasbeck@gmail.com>
2024-02-25 12:54:58 -0600

    Initial commit with my standard boilerplate so people know that this was written par moi

---
+++ enroll.bash
@@ -0,0 +1,27 @@
+ ################################################################################
+ # This code and its associated files were created at the instruction of        #
+ # professors at the University of Texas at San Antonio during my time as a     #
+ # student at the university. I, as a student, was not responsible for the idea #
+ # behind this code (i.e. project guidelines, functionality, and end purpose),  #
+ # but I, Matthew Thomas Beck, can confirm that myself and any project partners #
+ # (if applicable) were the ones responsible for writing it.                    #
+ ################################################################################
+
+
+
+
+
+ #############################################################
+ ############### IMPORT / DECLARE DEPENDENCIES ###############
+ #############################################################
+
+ # declare shebang line for bash interpreter
+ #!/bin/bash
+
+
+
+
+
+ #############################################################
+ ###############  ###############
+ #############################################################
